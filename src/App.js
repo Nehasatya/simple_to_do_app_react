64e861a656912935f013bb6356a6d0ef1120d14e
Neha Satya <nehasatya02@gmail.com>
2024-08-12 16:23:00 +0530

    added add,delete new task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Task } from './Task';
 
 function App() {
+
+  let [taskList,setTaskList] = useState([]);
+  // let task = "";
+  let [task,setTask] = useState("");
+  
+  const addItem = () => {
+    const newTask = {
+      id: taskList.length !== 0 ? taskList[taskList.length-1].id+1 : 1,
+      task: task,
+      completed: false
+    };
+    setTaskList([...taskList,newTask]);
+    console.log(taskList);
+  }
+
+  const extractValue = (event) =>{
+    // task = event.target.value;
+    setTask(event.target.value);
+  }
+
+  const deleteTask = (taskToDeleteID) => {
+    
+    console.log(taskList);
+    setTaskList(taskList.filter((task) => {
+      return (task.id !== taskToDeleteID)
+    }));    
+    console.log(taskList);
+  }
+
+  const completeTask = (completedTaskID) => {
+    setTaskList(taskList.map((task) => {
+      if(task.id === completedTaskID)
+        return {...task,completed: !task.completed};
+      else
+      return task;
+    }));
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addTask">
+        <input type="text" onChange={extractValue}></input>
+        <button onClick={addItem}>Add Task</button>
+      </div>
+      {/* {task} */} 
+      <div className="displayTask">
+        {taskList.map((task) => {
+          return( 
+            <Task taskName = {task.task} completed = {task.completed} completeTask = {completeTask} id = {task.id} deleteTask = {deleteTask} />
+        );
+        }) }       
+        </div>
     </div>
   );
 }
---
+++ src/Task.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const Task = (props) => {
+    return (
+        <div>              
+          <h1 style={{color: props.completed ? "green" : "black"}}> {props.taskName}</h1>
+          <button onClick = {() => {props.completeTask(props.id)}}>Complete</button>
+          <button onClick={() => props.deleteTask(props.id)}>X</button>
+            </div>
+    );
+}
